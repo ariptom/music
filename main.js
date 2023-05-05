4a7bfedd5c735d9d7327dfa686ecbf8ad75520a7
ariptom <95114854+ariptom@users.noreply.github.com>
2023-05-05 19:27:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+song="";
+function preload(){
+    song=loadSound("music.mp3");
+}
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,500,500);
+}
+function play(){
+    song.play();
+}
+
