7cc81ac24aa6c866dd1470a87025d533705ff868
ariptom <95114854+ariptom@users.noreply.github.com>
2023-05-14 14:14:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-song="";
+status="";
+song1="";
+song2="";
 LeftWristX=0;
 LeftWristY=0;
 
 RightWristX=0;
 RightWristY=0;
-
+scoreLeftWrist=0;
 function preload(){
-    song=loadSound("Heat-Waves(PagalWorld).mp3");
+    song1=loadSound("Heat-Waves(PagalWorld).mp3");
+    song2=loadSound("Unstoppable");
 }
 function setup(){
     canvas=createCanvas(500,500);
@@ -22,6 +25,10 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        console.log("Left Wrist Score = "+scoreLeftWrist)
+        
         LeftWristX=results[0].pose.LeftWrist.x;
         LeftWristY=results[0].pose.LeftWrist.y;
         console.log('lwx '+ LeftWristX + 'lwy ' + LeftWristY);
@@ -33,5 +40,19 @@ function gotPoses(results){
 }
 function draw(){
     image(video,0,0,500,500);
-}
 
+    fill("#FF0000");
+    stroke("#FF0000");
+
+        status=song1.isPlaying();
+        console.log(status);
+    if (scoreLeftWrist > 0.2){
+        circle(LeftWristX,LeftWristY,20);
+        song2.stop()
+
+    }
+    if (status="false"){
+        song1.play();
+        document.getElementById("heaing").innerHTML="Heat Waves";
+    }
+}
